4b96fc6155f2f45120de280cfba364a81036332e
MinkVie <vynguyen23200137@gmail.com>
2023-01-02 00:57:07 +0700

    commit Spring Web Mapping

---
+++ lab3/src/main/java/r2s/com/demo/SpringWebDemo/controller/AddressController.java
@@ -0,0 +1,54 @@
+
+package r2s.com.demo.SpringWebDemo.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import r2s.com.demo.SpringWebDemo.dto.request.InsertAddressRequestDTO;
+import r2s.com.demo.SpringWebDemo.entity.Address;
+import r2s.com.demo.SpringWebDemo.service.AddressService;
+
+import java.util.List;
+
+@RestController()
+@RequestMapping(value = "/address")
+public class AddressController {
+    @Autowired
+    private AddressService addressService;
+
+    @GetMapping
+    public ResponseEntity<?> getAllAddress() {
+        List<Address> addressList = addressService.getAllAddressDatabase();
+        return new ResponseEntity(addressList, HttpStatus.OK);
+    }
+    @PostMapping
+    public ResponseEntity insertAddress(@RequestBody InsertAddressRequestDTO requestDTO) {
+        Address address = addressService.insertAddress(requestDTO);
+        return new ResponseEntity(address, HttpStatus.OK);
+    }
+
+
+
+//    @PutMapping("/{address-id}")
+//    public ResponseEntity updateAddress(@PathVariable(value = "address-id") int addressId,
+//                                        @RequestBody UpdateAddressRequestDTO updateAddressRequestDTO) {
+//        AddressResponseDTO response = new AddressResponseDTO();
+//        response.setId(addressId);
+//        response.setName(updateAddressRequestDTO.getName());
+//        response.setPhone(updateAddressRequestDTO.getPhone());
+//        response.setProvince(updateAddressRequestDTO.getProvince());
+//        response.setDistrict(updateAddressRequestDTO.getDistrict());
+//        response.setStreet(updateAddressRequestDTO.getStreet());
+//        response.setType(updateAddressRequestDTO.isType());
+//        response.setDefaultAddress(updateAddressRequestDTO.isDefaultAddress());
+//        response.setAddressId(updateAddressRequestDTO.getAddressId());
+//
+//        return new ResponseEntity(response, HttpStatus.OK);
+//    }
+//
+//    @DeleteMapping("/{address-id}")
+//    public ResponseEntity deleteAddress(@PathVariable(value = "address-id") int addressId) {
+//        return new ResponseEntity(HttpStatus.NO_CONTENT);
+//    }
+}
---
+++ lab3/src/main/java/r2s/com/demo/SpringWebDemo/controller/CartController.java
@@ -0,0 +1,37 @@
+package r2s.com.demo.SpringWebDemo.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import r2s.com.demo.SpringWebDemo.dto.request.InsertCartRequestDTO;
+import r2s.com.demo.SpringWebDemo.entity.Cart;
+import r2s.com.demo.SpringWebDemo.service.CartService;
+
+import java.util.List;
+
+@RestController()
+@RequestMapping(value = "/cart")
+public class CartController {
+    @Autowired
+    private CartService cartService;
+
+    @GetMapping
+    public ResponseEntity<?> getAllCart() {
+        List<Cart> cartList = cartService.getAllCartDatabase();
+        return new ResponseEntity(cartList, HttpStatus.OK);
+    }
+    @PostMapping
+    public ResponseEntity insertCart(@RequestBody InsertCartRequestDTO requestDTO) {
+        Cart cart = cartService.insertCart(requestDTO);
+        return new ResponseEntity(cart, HttpStatus.OK);
+    }
+
+
+
+//
+//    @DeleteMapping("/{cart-id}")
+//    public ResponseEntity deleteCart(@PathVariable(value = "cart-id") int cartId) {
+//        return new ResponseEntity(HttpStatus.NO_CONTENT);
+//    }
+}
